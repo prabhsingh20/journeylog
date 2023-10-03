2d93c380cf45eb1ab8746d0beebb23e3099f7671
prabhsingh20 <singh20prabh@gmail.com>
2023-10-03 16:15:21 +0530

    Update url

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,8 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:7000";
+const BASE_URL = "https://prabhsingh20.github.io/worldwise/data/cities.json";
+// const BASE_URL = "http://localhost:7000";
 
 const CitiesContext = createContext();
 
